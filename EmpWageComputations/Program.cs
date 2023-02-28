3b71fef79dc14a3880931c8af91e00d4fe74492d
Bhagya Raju <bhagyarajusrivatsa12@gmail.com>
2023-02-28 22:51:36 +0530

    Welcome to EmpWageComputations

---
+++ EmpWageComputations/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpWageComputations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmpWageComputations");
+        }
+    }
+}
